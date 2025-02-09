ff24b525e632d5604c3e94e28d90cdf0ffc0a9ca
Daniel Posada <danielp0sada@javerianacali.edu.co>
2025-02-09 17:02:58 -0500

    NEW - Adición código que calcula el producto interno en el espacio de funciones y determina si un conjunto es ortogonal.

---
+++ Tareas/productPuntoEjr.py
@@ -0,0 +1,80 @@
+'''
+Ejercicio 1 Producto Punto 
+Daniel Alejandro Posada Noguera
+'''
+
+import numpy as np
+import sympy as sp
+import matplotlib.pyplot as plt
+
+def integrand(x, f, g):
+    return f(x) * g(x)
+
+def inner_product(f, g):
+    x = sp.Symbol('x')
+    integrand_expr = integrand(x, f, g)
+    result = sp.integrate(integrand_expr, (x, -sp.pi, sp.pi))
+    return result
+
+def are_all_ortogonals(set_vectors):
+    ans = True
+    i = 0
+    while i < len(set_vectors) and ans:
+        j = i + 1
+        while j < len(set_vectors) and ans:
+            if inner_product(set_vectors[i], set_vectors[j]) != 0:
+                ans = False
+            j += 1
+        i += 1
+    return ans  
+
+def rewrite_as_linear_combination(vector, set_vectors):
+    coefficients = []
+    for w_k in set_vectors:
+        numerator = inner_product(vector, w_k)
+        denominator = inner_product(w_k, w_k)
+        coefficient = numerator / denominator
+        coefficients.append(coefficient)
+    return coefficients
+
+def plot_funct_and_linear_combination(vec, set_vectors, coefficients):
+    x_vals = np.linspace(-np.pi, np.pi, 400)
+    y_original = [vec(x_val) for x_val in x_vals]
+
+    plt.plot(x_vals, y_original, label='Original function x^3', color='black')
+
+    y_combination = np.zeros_like(x_vals, dtype=float)
+    for i in range(len(coefficients)):
+        y_combination += coefficients[i] * np.array([float(set_vectors[i](x_val).evalf()) for x_val in x_vals], dtype=float)
+
+    plt.plot(x_vals, y_combination, label='Linear combination', color='red')
+
+    plt.legend()
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.title('Original function and its Linear Combination Approximation')
+    plt.show()
+
+def main():
+    x = sp.Symbol('x')
+    f1 = sp.Lambda(x, 1)
+    f2 = sp.Lambda(x, sp.cos(x))
+    f3 = sp.Lambda(x, sp.sin(x))
+    f4 = sp.Lambda(x, sp.cos(2*x))
+    f5 = sp.Lambda(x, sp.sin(2*x))
+    set_vectors = [f1, f2, f3, f4, f5]
+    
+    if are_all_ortogonals(set_vectors):
+        print("All vectors are orthogonal to each other.")
+        vec = sp.Lambda(x, x**3)
+        coefficients = rewrite_as_linear_combination(vec, set_vectors)
+        print(f"The coefficients to rewrite x^3 are: {coefficients}")
+
+        plot_funct_and_linear_combination(vec, set_vectors, coefficients)
+        
+    else:
+        print("Not all vectors are orthogonal to each other.")
+
+if __name__ == "__main__":
+    main()
